be9e136dbfeedde3f046a20997f296c4b85a1095
Margo <margo.bozhenova@gmail.com>
2020-05-18 01:53:28 +0300

    Add the ability to resize table columns and rows

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,25 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
+import { resizeHandler } from './table.resize';
+import { shouldResize } from './table.functions';
 
 export class Table extends ExcelComponent {
   static className = `excel__table`;
 
   constructor($root) {
-    super($root, { name: 'Table' });
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown'],
+    });
   }
 
   toHTML() {
     return createTable();
+  }
+
+  onMousedown(e) {
+    if (shouldResize(e)) {
+      resizeHandler(this.$root, e);
+    }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,22 +1,26 @@
 const CODES = { A: 65, Z: 90 };
 
-function toCell() {
+function toCell(_, col) {
   return `
-<div class="cell" contenteditable></div>
+<div class="cell" data-col="${col}" contenteditable></div>
 `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-  <div class="column">
+  <div class="column" data-type="resizable" data-col="${index}">
   ${col}
+  <div class="col-resize" data-resize="col"></div>
   </div>`;
 }
 
 function createRow(index, content) {
   return `
-<div class="row">
-  <div class="row-info">${index}</div>
+<div class="row" data-type="resizable">
+  <div class="row-info">
+  ${index}
+  ${index ? `<div class="row-resize" data-resize="row"></div>` : ``}
+  </div>
   <div class="row-data">${content}</div>
 </div>`;
 }
